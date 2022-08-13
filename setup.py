79ca725e31a0ad677853ba30bb44f6748ad235fc
YunhongHe <86810194+yunhonghe@users.noreply.github.com>
2022-08-13 13:46:29 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Conducted sentiment analysis on customer reviews by building, evaluating BERT models, produced predicted emotions on the customer reviews',
+    author='yunhonghe',
+    license='',
+)

179bbac07c0421d757aa5e0032488f35e39f45bd
kensukesuzuki17 <111254526+kensukesuzuki17@users.noreply.github.com>
2022-08-21 01:19:39 -0500

    Update recommendation_system.sh

--- recommendation_system.sh
+++ recommendation_system.sh
@@ -3,5 +3,5 @@ echo "This is the recommendation system pipeline"
 echo ""
 echo "Step 1: Predict recommended items for User 52354"
 echo ""
-python src/data/recommendation_system.py data/processed/reviews_with_user_id.csv 52354 data/processed/recommendation_for_user_52354.csv
+python src/data/recommendation_system.py data/processed/reviews_with_user_id.csv 52354 data/final/recommendation_for_user_52354.csv
 wait
